68a6cd55e394c31d5edf41854eb170542086cb23
Begimai <b.arstanbaeva@gmail.com>
2022-05-17 15:24:51 +0900

    Begimai commit search component

--- src/components/contexts/HouseContext.jsx
+++ src/components/contexts/HouseContext.jsx
@@ -18,7 +18,7 @@ const REDUCER = (state, action) => {
     case ACTIONS.GET_ALL_HOUSES:
       return { ...state, houses: action.payload };
     case ACTIONS.GET_HOUSE:
-      return { ...state, house: action.payload }
+      return { ...state, house: action.payload };
   }
 };
 
@@ -27,37 +27,37 @@ export const houseContext = createContext();
 const API = "http://localhost:8000/houses";
 
 const HouseContextProvider = ({ children }) => {
-
   const [state, dispatch] = useReducer(REDUCER, INIT_STATE);
 
   const getHouseById = async (id) => {
     const response = await axios.get(`${API}/${id}`);
     dispatch({
       type: ACTIONS.GET_HOUSE,
-      payload: response.data
-    })
-  }
-  
+      payload: response.data,
+    });
+  };
+
   const postHouse = (house) => {
-    const post = axios.post(API, house)
-    .then(() => {
-        window.location.href = `/houses`;
-    })
-  }
+    const post = axios.post(API, house).then(() => {
+      window.location.href = `/houses`;
+    });
+  };
 
   const getAllHouses = async () => {
-    const response = await axios.get(API);
+    const response = await axios.get(`${API}${window.location.search}`);
     dispatch({ type: ACTIONS.GET_ALL_HOUSES, payload: response.data });
   };
 
   return (
-    <houseContext.Provider value={{ 
-      houses: state.houses, 
-      house: state.house,
-      getAllHouses,
-      postHouse,
-      getHouseById,
-      }}>
+    <houseContext.Provider
+      value={{
+        houses: state.houses,
+        house: state.house,
+        getAllHouses,
+        postHouse,
+        getHouseById,
+      }}
+    >
       {children}
     </houseContext.Provider>
   );
